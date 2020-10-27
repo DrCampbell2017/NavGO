1b79589849fa77ed4167b06f0d9a94c32f67aed5
IanMC@bgsu.edu <IanMC@bgsu.edu>
2020-10-27 01:00:56 -0400

    Fixed Version of NavGO

---
+++ NavGO/Handler/NavGO_HandlerGO.go
@@ -0,0 +1,15 @@
+components {
+  id: "NavGO_HandlerScript"
+  component: "/NavGO/Handler/NavGO_HandlerScript.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
