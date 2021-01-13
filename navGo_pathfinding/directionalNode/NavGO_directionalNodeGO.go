bf2570c8192e0f302fab626d78bbf5dc325c57a4
IanMC@bgsu.edu <IanMC@bgsu.edu>
2021-01-12 22:59:55 -0500

    Changed directional nodes to have a label in order to identify them by their ID number during runtime. Added an example to demonstrate how to spawn multiple characters using the NavGo

--- navGo_pathfinding/directionalNode/NavGO_directionalNodeGO.go
+++ navGo_pathfinding/directionalNode/NavGO_directionalNodeGO.go
@@ -33,3 +33,57 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "IdNumber"
+  type: "label"
+  data: "size {\n"
+  "  x: 64.0\n"
+  "  y: 64.0\n"
+  "  z: 0.0\n"
+  "  w: 0.0\n"
+  "}\n"
+  "scale {\n"
+  "  x: 1.0\n"
+  "  y: 1.0\n"
+  "  z: 1.0\n"
+  "  w: 0.0\n"
+  "}\n"
+  "color {\n"
+  "  x: 0.0\n"
+  "  y: 0.0\n"
+  "  z: 0.0\n"
+  "  w: 1.0\n"
+  "}\n"
+  "outline {\n"
+  "  x: 0.0\n"
+  "  y: 0.0\n"
+  "  z: 0.0\n"
+  "  w: 1.0\n"
+  "}\n"
+  "shadow {\n"
+  "  x: 0.0\n"
+  "  y: 0.0\n"
+  "  z: 0.0\n"
+  "  w: 1.0\n"
+  "}\n"
+  "leading: 1.0\n"
+  "tracking: 0.0\n"
+  "pivot: PIVOT_CENTER\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  "line_break: false\n"
+  "text: \"#\"\n"
+  "font: \"/navGo_pathfinding/NavGOAssets/24-point-font.font\"\n"
+  "material: \"/builtins/fonts/label.material\"\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.1
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
